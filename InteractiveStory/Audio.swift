98995f29e58a7734d69e3e729c50bda5e2c71192
Sarah Kohl <sarahcharlottekohl@gmail.com>
2017-06-30 13:45:00 -0700

    All done: segues, keyboard support, sounds

---
+++ InteractiveStory/Audio.swift
@@ -0,0 +1,36 @@
+//
+//  Audio.swift
+//  InteractiveStory
+//
+//  Created by yuzu on 2017/06/29.
+//  Copyright © 2017 Sarah Kohl. All rights reserved.
+//
+
+import Foundation
+import AudioToolbox
+
+extension Story{
+    
+    var soundEffectName: String {
+        switch self {
+        case .droid, .home: return "HappyEnding"
+        case .monster: return "Ominous"
+        default: return "PageTurn"
+        }
+    }
+
+    var soundEffectURL: URL {
+        let path = Bundle.main.path(forResource: soundEffectName, ofType: "wav")!
+        return URL(fileURLWithPath: path)
+    }
+}
+
+class SoundEffectsPlayer {
+    var sound: SystemSoundID = 0
+    
+    func playSound(for story: Story){
+        let soundURL = story.soundEffectURL as CFURL
+        AudioServicesCreateSystemSoundID(soundURL, &sound)
+        AudioServicesPlaySystemSound(sound)
+    }
+}
--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "startAdventure" {
+            
+            do{
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw AdventureError.nameNotProvided
+                    } else {
+                        guard let pageController = segue.destination as? PageController else {
+                            return
+                        }
+                        
+                        pageController.page = Adventure.story(withName: name)
+                    }
+                }
+            } catch AdventureError.nameNotProvided {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Please enter your name to begin playing.", preferredStyle: .alert)
+                
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(action)
+                
+                present(alertController, animated: true, completion: nil)
+            } catch let error {
+                fatalError("\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    
+    
+    func keyboardWillShow(_ notification: Notification) {
+        if let info = notification.userInfo, let keyboardFrame = info[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let frame = keyboardFrame.cgRectValue
+            textFieldBottomConstraint.constant = frame.size.height + 10
+            
+            UIView.animate(withDuration: 0.8) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    
+    func keyboardWillHide(_ notification: Notification) {
+        textFieldBottomConstraint.constant = 40.0
+        
+        UIView.animate(withDuration: 0.8){
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
+}
+
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
